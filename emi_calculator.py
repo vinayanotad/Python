2ced8192b4668f76a17b2c6e35c90e61774cf66f
vinayanotad <chinnunotad2001gmail.com>
2023-04-15 18:58:02 +0530

    EMI Calculator

--- emi_calculator.py
+++ emi_calculator.py
@@ -21,32 +21,43 @@ def calculate():
     emi_label.config(text="EMI: Rs. {:.2f}".format(emi))
     
 
+    
+    # paymentvalue = emi* tenure
+    # totalinterest_value = paymentvalue - principal
+    
+    # totalinterest_value_label.config(text="Total Interest: {:.2f}".format(totalinterest_value ))
+    
+    
+
 # create a window
 window = tk.Tk()
 window.title("EMI Calculator")
+window.geometry("900x900")
+window.configure(bg='lightblue')
+
 
 # adding a label to the root window
-lbl = tk.Label(window, text = "EMI calculator",fg='blue')
+lbl = tk.Label(window, text = "EMI calculator",fg='blue',font=('bold',30))
 lbl.pack()
 
 #  create input fields
 
-principal_label = tk.Label(window, text="Principal (Rs.):")
+principal_label = tk.Label(window, text="Principal (Rs.):",font=('arial',17))
 principal_label.pack()
 principal_entry = tk.Entry(window)
 principal_entry.pack()
 
-interest_label = tk.Label(window, text="Interest Rate (% p.a.):")
+interest_label = tk.Label(window, text="Interest Rate (% p.a.):",font=('arial',17))
 interest_label.pack()
 interest_entry = tk.Entry(window)
 interest_entry.pack()
 
-downpayment_label = tk.Label(window, text="Down Payment(Mention if 0):")
+downpayment_label = tk.Label(window, text="Down Payment(Mention if 0):",font=('arial',17))
 downpayment_label.pack()
 downpayment_entry = tk.Entry(window)
 downpayment_entry.pack()
 
-tenure_label = tk.Label(window, text="Time Period (months):")
+tenure_label = tk.Label(window, text="Time Period (months):",font=('arial',17))
 tenure_label.pack()
 tenure_entry = tk.Entry(window)
 tenure_entry.pack()
@@ -55,15 +66,22 @@ tenure_entry.pack()
 
 
 # create a button to calculate EMI
-calculate_button = tk.Button(window, text="Calculate EMI",fg='Green',command=calculate)
+calculate_button = tk.Button(window, text="Calculate EMI",fg='Green',font=('bold',20),command=calculate)
 calculate_button.pack()
+
 
 # clear_button = tk.Button(window, text="CLEAR",fg='red',command=clear)
 # clear_button.pack()
 
 # create a label to display EMI
-emi_label = tk.Label(window)
+emi_label = tk.Label(window,bg='orange',font=('arial',19))
 emi_label.pack()
+
+
+#create a label to display total_interest_value
+# totalinterest_value_label = tk.Label(window,bg='orange',font=('arial',17))
+# totalinterest_value_label.pack()
+
 
 # start the window
 window.mainloop()
